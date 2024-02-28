84f6bb60130d3529d744f129f76bfd944d424e6d
Rayhanvin <143564681+Rayhanvin@users.noreply.github.com>
2024-02-28 20:57:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ const showMenu = (toggleId, navId) =>{
  }
  
  showMenu('nav-toggle','nav-menu')
+
+ let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
